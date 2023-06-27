e2ad60184614382e7bbdb49ac8bc5090965e8064
Natasa <nkilibarda3@gmail.com>
2023-06-27 17:50:59 +1000

    Add create-swap page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,10 +50,16 @@ router.get('/swap/:id', async (req, res) => {
   }
 });
 
+router.get('/create-swap', withAuth, (req, res) => {
+  res.render('create-swap', {
+    logged_in: req.session.logged_in,
+  });
+});
+
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/');
     return;
   }
 
@@ -63,7 +69,7 @@ router.get('/login', (req, res) => {
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/');
     return;
   }
 
