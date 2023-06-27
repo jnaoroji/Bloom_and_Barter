a699587e4d4a9dc0c85b713e528c255671e20d97
Natasa <nkilibarda3@gmail.com>
2023-06-27 17:27:17 +1000

    remove route for profile (for now)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,27 +50,6 @@ router.get('/swap/:id', async (req, res) => {
   }
 });
 
-//get swap page if logged in
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Swap }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
